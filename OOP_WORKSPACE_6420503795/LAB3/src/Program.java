aaa5e9188dd2e12a178e543cd31217ec4d4869d7
paiizxqh <103915763+paiizxqh@users.noreply.github.com>
2022-07-22 12:39:47 +0700

    Add files via upload

---
+++ OOP_WORKSPACE_6420503795/LAB3/src/Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		Vector v1 = new Vector();
+		
+		Vector v2 = ;
+		
+		System.out.print("Vector v1: ");
+		
+		System.out.print("Vector v2: ");
+
+	}
+
+}
