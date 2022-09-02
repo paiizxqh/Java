917d29f58a0bbb12cd1a62921c27f93d792c4422
paiizxqh <103915763+paiizxqh@users.noreply.github.com>
2022-09-02 12:05:51 +0700

    Add files via upload

---
+++ Lab8/src/Lab8_3/JFrame.java
@@ -0,0 +1,13 @@
+package Lab8_3;
+
+public class JFrame {
+
+	public static void main(String[] args) {
+		JFrame fram = new JFrame("Test");
+		frame.setSize(250,300);
+		frame.setVisible(true);
+		frame.setEnabled(true);
+
+	}
+
+}
